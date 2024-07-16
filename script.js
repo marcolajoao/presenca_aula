f60d221143e3fe65220e9751c6be62568064d889
marcolajoao <166566581+marcolajoao@users.noreply.github.com>
2024-07-15 21:25:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,30 @@ document.getElementById('downloadForm').addEventListener('submit', function(even
 
     const name = document.getElementById('name').value;
     if (name) {
-        const link = document.createElement('a');
-        link.href = 'certificado_padrao.jpg'; // Caminho do repositório
-        link.download = `image_for_${name}.jpg`; // Nome do arquivo apos o download
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
+        const canvas = document.getElementById('canvas');
+        const context = canvas.getContext('2d');
+        const image = new Image();
+
+        image.onload = function() {
+            canvas.width = image.width;
+            canvas.height = image.height;
+            context.drawImage(image, 0, 0);
+
+            // Configurações de texto
+            context.font = '40px Arial';
+            context.fillStyle = 'white';
+            context.textAlign = 'center';
+            context.fillText(name, canvas.width / 2, canvas.height - 50);
+
+            const link = document.createElement('a');
+            link.href = canvas.toDataURL('image/jpeg');
+            link.download = `image_with_${name}.jpg`;
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        };
+
+        image.src = 'certificado_padrao.jpg';
     } else {
         alert('Please enter your name.');
     }
